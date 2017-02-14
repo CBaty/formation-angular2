ca70a3ebca08fc6be4e41ee342e78f5f3092b0ef
Arnaud Tournier <ltearno@gmail.com>
2017-02-14 18:41:56 +0100

    yo

--- projets/HelloWorld/app.component.ts
+++ projets/HelloWorld/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core'
+import { APP_CONFIG } from './app.config'
 @Component({
     selector: 'angular-app',
-    template: '<h1>Hello copain !</h1>'
+    template: '<h1>Hello copain ! {{config | json}}</h1>'
 })
 export class AngularAppComponent {
+    constructor( @Inject(APP_CONFIG) private config: { production; title; }) {
+    }
 }
--- projets/HelloWorld/app.module.ts
+++ projets/HelloWorld/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AngularAppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AngularAppComponent } from './app.component'
+import { APP_CONFIG } from './app.config'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [AngularAppComponent],
+    providers: [
+        { provide: 'CONSTANTE', useValue: 5 },
+        { provide: APP_CONFIG, useValue: { production: false, title: 'Sandrero' } }
+    ],
     bootstrap: [AngularAppComponent]
 })
 export class AppModule {
